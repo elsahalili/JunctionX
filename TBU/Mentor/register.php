3719292747f69d6fbaab4311ce8ce1603a4e9feb
elsahalili <elsahalili84@gmail.com>
2025-05-03 01:39:19 +0200

    .php files

--- TBU/Mentor/register.php
+++ TBU/Mentor/register.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
       echo "<script>
         alert('Welcome, $name! You have signed up successfully.');
-        window.location.href = 'index.html';
+        window.location.href = 'index.php';
       </script>";
       exit();
     }
